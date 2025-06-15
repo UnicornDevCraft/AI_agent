2433e376ae295d7a92445acc6224f5dc02e66018
UnicornDevCraft <kseniia.usyk@outlook.com>
2025-06-15 23:09:02 +0200

    For review

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-# Just comment the unnecessary before running the file
+# Just uncomment the necessary before running the file
 
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
@@ -6,24 +6,24 @@ from functions.write_files import write_file
 from functions.run_python_file import run_python_file
 
 
-print(get_files_info("calculator"))
-print(get_files_info("calculator", "pkg"))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
+#print(get_files_info("calculator"))
+#print(get_files_info("calculator", "pkg"))
+#print(get_files_info("calculator", "/bin"))
+#print(get_files_info("calculator", "../"))
 
-print(get_file_content("calculator", "main.py"))
-print(get_file_content("calculator", "pkg/calculator.py"))
-print(get_file_content("calculator", "/bin/cat"))
-print(get_file_content("calculator", "lorem.txt"))
+#print(get_file_content("calculator", "main.py"))
+#print(get_file_content("calculator", "pkg/calculator.py"))
+#print(get_file_content("calculator", "/bin/cat"))
+#print(get_file_content("calculator", "lorem.txt"))
 
-content = get_file_content("calculator", "main.py")
-print(write_file("calculator", "main.py", content))
-print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
-print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+#content = get_file_content("calculator", "main.py")
+#print(write_file("calculator", "main.py", content))
+#print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+#print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+#print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
-print(run_python_file("calculator", "main.py"))
-print(run_python_file("calculator", "tests.py"))
-print(run_python_file("calculator", "../main.py"))
-print(run_python_file("calculator", "nonexistent.py"))
+#print(run_python_file("calculator", "main.py"))
+#print(run_python_file("calculator", "tests.py"))
+#print(run_python_file("calculator", "../main.py"))
+#print(run_python_file("calculator", "nonexistent.py"))
 
